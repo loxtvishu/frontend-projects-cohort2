b13c0f3263f377f60b75cd212e2d8e991aa33924
loxtvishu <2003vishusaini@gmail.com>
2026-02-24 21:18:34 +0530

    Sucessfully make the Snake Board

--- Project-8(Snake Game)/script.js
+++ Project-8(Snake Game)/script.js
@@ -1,15 +1,39 @@
 const board = document.querySelector(".board");
-const blockHeight = 80
-const blockWidth = 80
+const blockHeight = 50
+const blockWidth = 50
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
+
+const blocks = [];
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const block = document.createElement("div")
         block.classList.add("block");
         board.appendChild(block);
-        
+        blocks[`${row}-${col}`] = block
     }
 }
+
+const snake = [{
+    x: 2, y: 3
+},
+{
+    x: 2, y: 4
+},
+{
+    x: 2, y: 5
+}]
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+
+    })
+}
+
+setInterval(() => {
+    render();
+}, 300)
+
